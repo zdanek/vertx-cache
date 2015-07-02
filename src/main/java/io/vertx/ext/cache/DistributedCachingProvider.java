432471c6bd9542e336c2230e87d8d1c2132288d2
Bartek Zdanowski <bzd@touk.pl>
2015-07-03 00:03:34 +0200

    wip

---
+++ src/main/java/io/vertx/ext/cache/DistributedCachingProvider.java
@@ -0,0 +1,7 @@
+package io.vertx.ext.cache;
+
+/**
+ * Created by bartek on 03.07.15.
+ */
+public class DistributedCachingProvider {
+}
--- src/test/java/io/vertx/ext/cache/CacheExample.java
+++ src/test/java/io/vertx/ext/cache/CacheExample.java
@@ -1,7 +1,9 @@
 package io.vertx.ext.cache;
 
+import javax.cache.Cache;
 import javax.cache.CacheManager;
 import javax.cache.Caching;
+import javax.cache.configuration.MutableConfiguration;
 import javax.cache.spi.CachingProvider;
 
 import io.vertx.core.Vertx;
@@ -16,6 +18,16 @@ public class CacheExample {
 
         final CachingProvider cachingProvider = Caching.getCachingProvider();
         final CacheManager cacheManager = cachingProvider.getCacheManager();
+
+        MutableConfiguration<Object, Object> config = new MutableConfiguration<>();
+        Cache<Object, Object> cache = cacheManager.createCache("test", config);
+
+        cache.put("key", "value");
+
+        assert "value".equals(cache.get("key"));
+
+
+        DistributedCachingProvider provider = DistributedCachingProvider.getCachingProvider(vertx);
 
     }
 }
